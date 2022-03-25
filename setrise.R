0ad1350581d52eecd16f70eb631f832fbecd5b9e
dipperGit <shirakawa_hokuto26@fra.go.jp>
2022-03-25 15:48:29 +0900

    ver1更新 結果の出力時に、最初の行を改行するように変更

--- setrise.R
+++ setrise.R
@@ -41,6 +41,6 @@ time$dsunrise <- as.character(time$sunrise)
 time$dsunrise <- substring(time$dsunrise, nchar(time$dsunrise)-7, nchar(time$dsunrise))
 
 #結果を表示
-cat(paste( now, "の", as.factor(tar$pref), "の", "\n","日の出時間は", time$dsunrise, "\n", "日の入り時間は", time$dsunset, "です。", sep="" ) )
+cat(paste("\n", now, "の", as.factor(tar$pref), "の", "\n","日の出時間は", time$dsunrise, "\n", "日の入り時間は", time$dsunset, "です。", sep="" ) )
 
 }#おわりんこ
