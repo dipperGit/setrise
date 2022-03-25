12ed7b77409c2de198073f68a8c763fd7ff881aa
dipperGit <shirakawa_hokuto26@fra.go.jp>
2022-03-25 15:36:56 +0900

    とりま

--- setrise.R
+++ setrise.R
@@ -1,4 +1,7 @@
 #setrise関数
+#都道府県名を入力すれば、実行日前後日の出日の入り時刻を計算できる。
+
+#調査や釣りなどに活躍！（?）
 
 #都道府県ごとの緯度経度入力
 pref <- c("北海道","青森県","岩手県","宮城県","秋田県","山形県", "福島県","茨城県","栃木県","群馬県","埼玉県","千葉県","東京都","神奈川県","新潟県","富山県","石川県","福井県","山梨県","長野県","岐阜県","静岡県","愛知県","三重県","滋賀県","京都府","大阪府","兵庫県","奈良県","和歌山県", "鳥取県","島根県","岡山県","広島県","山口県","徳島県","香川県","愛媛県","高知県","福岡県","佐賀県","長崎県","熊本県","大分県","宮崎県","鹿児島県", "沖縄県" )
@@ -10,3 +13,17 @@ df <- data.frame(pref = pref, lat = lat, lon = lon)
 #パッケージの読み込みはpkgch関数で管理
 source("https://raw.githubusercontent.com/dipperGit/pkgch/br1/pkgch.R", encoding="UTF-8")
 pkgch("suncalc")
+
+now <- Sys.Date()
+#手動の場合は、
+#now <- as.POSIXct("2022-03-25")
+#のように書く。
+
+#loc <- "北海道"
+tar <- df[df$pref == loc,]
+
+time <- getSunlightTimes(date=now, lat = tar$lat[1], lon = tar$lon[1], tz="JAPAN" )
+time$tday <- 
+
+
+cat(paste( now, "の", as.factor(tar$pref), "の", "\n","日の出時間は", time$sunrise, "\n", "日の入り時間は", time$sunset, "です。", sep="" ) )
